28301729d57a5f9f6b41f94d70072468c1a8e1f1
xihadulislam <xihad.bd@gmail.com>
2021-10-04 21:04:13 +0600

    init commit

---
+++ app/src/main/java/com/project/wordlearner/ui/splash/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.project.wordlearner.ui.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.project.wordlearner.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
