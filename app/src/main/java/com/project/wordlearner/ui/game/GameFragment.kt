e798fb27ccde3b5cd8a9cfb23eb3538dc18fa791
xihadulislam <xihad.bd@gmail.com>
2021-10-05 01:23:56 +0600

    game

--- app/src/main/java/com/project/wordlearner/ui/game/GameFragment.kt
+++ app/src/main/java/com/project/wordlearner/ui/game/GameFragment.kt
@@ -4,41 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.project.wordlearner.databinding.FragmentNotificationsBinding
+import com.project.wordlearner.R
+import com.project.wordlearner.ui.dashboard.DashboardViewModel
 
 class GameFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: GameViewModel
-    private var _binding: FragmentNotificationsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        notificationsViewModel =
-            ViewModelProvider(this).get(GameViewModel::class.java)
-
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+    companion object {
+        fun newInstance() = GameFragment()
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private lateinit var viewModel: GameViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+
+
     }
 }
