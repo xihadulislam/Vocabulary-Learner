69159876646a2bbb3006a0cf4f8e595fb6c9a3d2
xihadulislam <xihad.bd@gmail.com>
2021-10-06 22:54:50 +0600

    added search_fragment.xml

--- app/src/main/java/com/project/wordlearner/ui/main/MainActivity.kt
+++ app/src/main/java/com/project/wordlearner/ui/main/MainActivity.kt
@@ -27,6 +27,7 @@ class MainActivity : AppCompatActivity() {
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home,
+                R.id.navigation_search,
                 R.id.navigation_dashboard,
                 R.id.navigation_game,
                 R.id.navigation_profile
---
+++ app/src/main/java/com/project/wordlearner/ui/search/SearchViewModel.kt
@@ -0,0 +1,7 @@
+package com.project.wordlearner.ui.search
+
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
