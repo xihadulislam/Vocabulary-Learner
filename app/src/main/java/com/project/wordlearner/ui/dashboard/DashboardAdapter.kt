aedf7e4fe62cd33e3f312e5d9e77cf63b378d88e
xihadulislam <xihad.bd@gmail.com>
2021-10-07 22:21:59 +0600

    search ui done

---
+++ app/src/main/java/com/project/wordlearner/ui/dashboard/DashboardAdapter.kt
@@ -0,0 +1,48 @@
+package com.project.wordlearner.ui.dashboard
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.project.wordlearner.R
+import com.project.wordlearner.data.models.Data
+
+class DashboardAdapter(val context: Context) :
+    RecyclerView.Adapter<DashboardAdapter.MyViewHolder>() {
+
+    private var mList: MutableList<String> = mutableListOf()
+
+    fun setUpdatedList(list: List<String>) {
+        mList.clear()
+        mList.addAll(list)
+        notifyDataSetChanged()
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view: View =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_category, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bindView(mList[position], context)
+    }
+
+    override fun getItemCount(): Int {
+        return if (mList.isNullOrEmpty()) 0 else mList.size
+    }
+
+
+    class MyViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+
+        fun bindView(faq: String, context: Context) {
+
+        }
+
+    }
+
+}
--- app/src/main/java/com/project/wordlearner/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/project/wordlearner/ui/dashboard/DashboardFragment.kt
@@ -1,15 +1,22 @@
 package com.project.wordlearner.ui.dashboard
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.littlemango.stacklayoutmanager.DefaultAnimation
+import com.littlemango.stacklayoutmanager.StackLayoutManager
 import com.project.wordlearner.R
 import com.project.wordlearner.databinding.FragmentDashboardBinding
+import com.project.wordlearner.ui.home.HomeAdapter
 import com.project.wordlearner.ui.home.HomeFragment
 import com.project.wordlearner.ui.home.HomeViewModel
 
@@ -21,6 +28,10 @@ class DashboardFragment : Fragment() {
 
     private lateinit var viewModel: DashboardViewModel
 
+    private lateinit var recyclerView: RecyclerView
+
+    private lateinit var adapter: DashboardAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -31,6 +42,27 @@ class DashboardFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
+
+        recyclerView = view.findViewById(R.id.dashboardList)
+        adapter = DashboardAdapter(requireContext())
+
+        recyclerView.also {
+            it.setHasFixedSize(true)
+            it.setItemViewCacheSize(20)
+            ViewCompat.setNestedScrollingEnabled(it, false)
+            it.layoutManager = LinearLayoutManager(requireContext())
+            it.adapter = adapter
+        }
+
+        val list = mutableListOf<String>()
+        list.add("trtertre")
+        list.add("trtertre")
+        list.add("trtertre")
+        list.add("trtertre")
+        list.add("trtertre")
+        list.add("trtertre")
+
+        adapter.setUpdatedList(list)
 
 
     }
